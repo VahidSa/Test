2823bae765c689dc455f7606e6e3de71bd3ac9e0
VM <vahidsanejad@gmail.com>
2016-05-23 16:18:32 +0430

    bug fixed

--- prog.cpp
+++ prog.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
-int main(){
-	num=input();
-	return 0;	
-}
 int input(){
 	int n;
 	cout<<"Enter Number";
 	cin>>n;
 	return n;
 }
+int main(){
+	int num;
+	num=input();
+	return 0;	
+}
+
